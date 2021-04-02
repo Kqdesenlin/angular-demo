95bfc19df28484906b256131a71c4f52ca1bc7a3
kqdesenlin <1194999721@qq.com>
2021-04-02 11:29:13 +0800

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideTreeComponent } from './side-tree/side-tree.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { GridListComponent } from './grid-list/grid-list.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideTreeComponent,
+    HeroesListComponent,
+    PageNotFoundComponent,
+    GridListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatSliderModule,
+    MatTreeModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    ScrollingModule,
+    RouterModule.forRoot([
+      { path: 'side-tree', component: SideTreeComponent },
+      { path: 'heroes-list', component: HeroesListComponent },
+      { path: '', redirectTo: '/heroes-list', pathMatch: 'full' },
+      { path: 'grid-list', component: GridListComponent },
+      { path: '**', component: PageNotFoundComponent },
+    ]),
+    BrowserAnimationsModule,
+  ],
+  exports: [
+
   ],
   providers: [],
   bootstrap: [AppComponent]
