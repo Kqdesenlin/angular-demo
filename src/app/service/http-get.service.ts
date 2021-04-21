b4be080a79ee42bdd6c757b2623e2e8855306690
kqdesenlin <1194999721@qq.com>
2021-04-21 11:31:33 +0800

    部分修复httpsession

--- src/app/service/http-get.service.ts
+++ src/app/service/http-get.service.ts
@@ -4,11 +4,11 @@ import { HttpHeaders } from '@angular/common/http';
 
 import { OperateResult, FakeNode, UpdateTableInfo } from './operate-result';
 
-
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
-    Authorization: 'my-auth-token'
+    Authorization: 'my-auth-token',
+    'Connection': 'keep-alive'
   })
 };
 @Injectable()
@@ -16,7 +16,6 @@ export class HttpGetService {
   getSqlPath = 'http://localhost:8080/api/sql';
   operateResult = '';
   getRtn = '';
-  headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*' })
   constructor(private http: HttpClient,
   ) {
   }
@@ -26,7 +25,7 @@ export class HttpGetService {
       .set("sql", sql);
 
     let temp: OperateResult;
-    this.http.post<OperateResult>(this.getSqlPath, { "sql": sql })
+    this.http.post<OperateResult>(this.getSqlPath, { "sql": sql }, httpOptions)
       .subscribe((data: OperateResult) => {
         temp.info = data.info;
         temp.rtn = data.rtn;
@@ -38,7 +37,7 @@ export class HttpGetService {
     return this.http.get<FakeNode[]>("http://localhost:8080/api/data");
   }
   postSql(sql: string) {
-    return this.http.post<OperateResult[]>(this.getSqlPath, { "sql": sql });
+    return this.http.post<OperateResult[]>(this.getSqlPath, { "sql": sql }, httpOptions);
   }
   getUpdateTableData() {
     return this.http.get<UpdateTableInfo[]>("http://localhost:8080/api/updatedata");
